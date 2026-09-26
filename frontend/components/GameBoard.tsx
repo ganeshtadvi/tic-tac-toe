b8a5e5e284b8bd5dc39ec174166bb1a5cf90f3cd
ganesh <ganeshtadvi21@gmail.com>
2026-09-26 22:04:48 +0530

    check winner funcion added

--- frontend/components/GameBoard.tsx
+++ frontend/components/GameBoard.tsx
@@ -6,6 +6,30 @@ import Square from "./Square";
 const GameBoard = () => {
   const [board, setBoard] = useState<string[]>(Array(9).fill(""));
   const [isInputX, setInputX] = useState(true);
+  const [clickCount, setClickCount] = useState(0);
+
+  const checkWinner = (board: string[]) => {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of winningPatterns) {
+      if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+
+    return null;
+  };
 
   const handleBtnClick = (index: number) => {
     if (board[index] !== "") {
@@ -16,11 +40,23 @@ const GameBoard = () => {
 
     newBoard[index] = isInputX ? "X" : "O";
 
+    setClickCount(clickCount + 1);
+
     setBoard(newBoard);
 
     setInputX(!isInputX);
 
     console.log(newBoard);
+
+    if (clickCount + 1 >= 5) {
+      const result = checkWinner(newBoard);
+
+      if (!result) {
+        console.log(null);
+      } else {
+        console.log(result);
+      }
+    }
   };
 
   return (
