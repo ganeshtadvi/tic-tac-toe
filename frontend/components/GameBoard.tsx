4ffacd11be6d05b428f6fb273984bbdf589a2ea1
ganesh <ganeshtadvi21@gmail.com>
2026-09-26 16:05:10 +0530

    create board and boxes, get input x and o after clicked

--- frontend/components/GameBoard.tsx
+++ frontend/components/GameBoard.tsx
@@ -4,27 +4,33 @@ import { useState } from "react";
 import Square from "./Square";
 
 const GameBoard = () => {
-  const [isInputX, setInputX] = useState(false);
-  const [boxValue, setBoxValue] = useState("O");
+  const [board, setBoard] = useState<string[]>(Array(9).fill(""));
+  const [isInputX, setInputX] = useState(true);
 
-  const handleSquareClick = () => {
-    if (isInputX) {
-      setBoxValue("X");
-      setInputX(false);
-    } else {
-      setBoxValue("O");
-      setInputX(true);
+  const handleBtnClick = (index: number) => {
+    if (board[index] !== "") {
+      return;
     }
+
+    const newBoard = [...board];
+
+    newBoard[index] = isInputX ? "X" : "O";
+
+    setBoard(newBoard);
+
+    setInputX(!isInputX);
+
+    console.log(newBoard);
   };
 
   return (
     <div className="game-board">
-      {Array.from({ length: 9 }, (_, index) => (
+      {board.map((value, index) => (
         <Square
           key={index}
-          onClick={handleSquareClick}
-          boxValue={boxValue}
-          //  setInputX={setInputX}
+          boxValue={value}
+          btnIndex={index}
+          onClick={handleBtnClick}
         />
       ))}
     </div>
--- frontend/components/Square.tsx
+++ frontend/components/Square.tsx
@@ -2,14 +2,24 @@ import "./Square.css";
 
 type SquareProps = {
   boxValue: string;
-  onClick: () => void;
+  btnIndex: number;
+  onClick: (index: number) => void;
 };
 
-const Square = ({ boxValue, onClick }: SquareProps) => {
+const Square = ({ boxValue, btnIndex, onClick }: SquareProps) => {
+  const handleBtnClick = () => {
+    if (boxValue !== "") {
+      return;
+    }
+
+    onClick(btnIndex);
+  };
+
   return (
-    <button className="square" onClick={onClick}>
+    <button className="square" onClick={handleBtnClick}>
       {boxValue}
     </button>
   );
 };
+
 export default Square;
